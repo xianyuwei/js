407b83b30e305f2f6fbad7067bab874c24624be6
weixy <xianyu.wei@nxp.com>
2018-08-30 13:51:41 +0800

    improve get

--- SimpleHTTPServerReadProc.py
+++ SimpleHTTPServerReadProc.py
@@ -21,6 +21,29 @@ from SimpleHTTPServer import SimpleHTTPRequestHandler
 import BaseHTTPServer
 
 igh_proc_file_path = '/proc/ethercat/igh_ec_performance_log'
+
+save_file_flag = 0
+
+try:
+    save_file = open('1.txt', 'r')
+    save_file_flag = 1
+except IOError:
+    save_file_flag = 0
+    print "save_file is not accessible."
+
+def get_save_file_info():
+    if (save_file_flag == 0):
+        return '0\t0\t0\t0'
+    buf = save_file.readline()
+    offset = save_file.tell()
+
+    tmp_buf = save_file.readline()
+    if not tmp_buf:
+        save_file.seek(0)
+    else:
+        save_file.seek(offset)
+    return buf;
+
 class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
@@ -28,19 +51,31 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
 		
     def do_GET (self):
         """Serve a GET request."""
+        save_buf = get_save_file_info()
         try:
-            f =open(igh_proc_file_path, 'r')
-            buf = f.readline()
-            f.close()
+            proc_file =open(igh_proc_file_path, 'r')
+            proc_buf = proc_file.readline()
+            proc_buf = proc_buf.strip('\n')
+            proc_file.close()
         except IOError:
-            buf = '0\t0\t0\t0'
-            print "File is not accessible."
+            proc_file = '0\t0\t0\t0'
+            print "proc_file is not accessible."
+        buf = proc_buf + '\t' + save_buf
         self.protocal_version = 'HTTP/1.1'
         self.send_response(200)  
         self.send_header("Welcome", "Contect")         
         self.end_headers()  
         self.wfile.write(buf)
-
+    def do_POST(self):
+        """Serve a POST request."""
+        self.protocal_version = 'HTTP/1.1'
+        self.send_response(200)
+        self.send_header("Welcome", "Contect")         
+        self.end_headers()
+        datas=self.rfile.read(int(self.headers['content-length']))
+        print datas
+ 
+ 
 
 if __name__ == '__main__':
     BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
